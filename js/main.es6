e568ff5c95d28b66af778a28c7b43e6aab851095
Fancy Icarus <lucasicarus@hotmail.com>
2016-04-25 23:33:02 +0800

    Home 16.04.25 Kind of finished

--- js/main.es6
+++ js/main.es6
@@ -1,51 +1,102 @@
+const W_4 = Math.floor(wid / 4),
+    W_6 = Math.floor(wid / 6),
+    W_7 = Math.floor(wid / 7),
+    W_10 = Math.floor(wid / 10),
+    W_15 = Math.floor(wid / 15),
+    W_2_3 = Math.floor(wid * 2 / 3),
+    W_4_5 = Math.floor(wid * 4 / 5),
+    W_5_6 = Math.floor(wid * 5 / 6),
+    W_7_26 = Math.floor(wid * 7 / 26),
+    W_5_12 = Math.floor(wid * 5 / 12),
+
+    H_2 = Math.floor(hei / 2),
+    H_25 = Math.floor(hei / 25),
+    H_2_5 = Math.floor(hei * 2 / 5),
+    H_7_10 = Math.floor(hei * 7 / 10),
+    H_11_16 = Math.floor(hei * 11 / 16),
+    H_5_16 = Math.floor(hei * 5 / 16),
+    H_5_18 = Math.floor(hei * 5 / 18),
+    H_5_24 = Math.floor(hei * 5 / 24),
+    H_11_25 = Math.floor(hei * 11 / 25),
+    H_11_60 = Math.floor(hei * 11 / 60),
+    H_45_64 = Math.floor(hei * 45 / 64),
+    H_200_371 = Math.floor(hei * 200 / 371);
+
+// 公共变量和一些常量。
 let images = [],
     init = [],
-    resule = 0,
+    result = {},
     load = 0,
     ctx = cav.getContext('2d');
 
-const x_l = Math.floor(wid / 6),
-    y_l = Math.floor(hei * 2 / 5),
-    wid_l = Math.floor(wid * 2 / 3),
-    hei_l = Math.floor(hei / 25);
-
-window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame ||
+// 工具函数
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        window.oRequestAnimationFrame      ||
-        window.msRequestAnimationFrame     ||
-        function( callback ){
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
             window.setTimeout(callback, 1000 / 60);
         };
 })();
 
-function getTouchendPosition(event){
+function drawRoundedRect(x, y, width, height, radius, context) {
 
-    let x = event.changedTouches[0].clientX;
-    let y = event.changedTouches[0].clientY;
-
-    return {x, y};
-}
-
-function fillRoundedRect(context, x, y, width, height, radius) {
     context.beginPath();
-    
+
     if (width > 0) {
         context.moveTo(x + radius, y);
     } else {
         context.moveTo(x - radius, y);
     }
+
     context.arcTo(x + width, y, x + width, y + height, radius);
     context.arcTo(x + width, y + height, x, y + height, radius);
     context.arcTo(x, y + height, x, y, radius);
+
     if (width > 0) {
         context.arcTo(x, y, x + width, y, radius);
     } else {
         context.arcTo(x, y, x - width, y, radius);
     }
+}
+
+function fillRoundedRect(x, y, width, height, style = "#ffffff", radius = 5, context = ctx) {
+    context.fillStyle = style;
+    drawRoundedRect(x, y, width, height, radius, context);
+    context.fill();
+}
+
+function roundedRectDot(x, y, width, height, rounded, color, radius, interval, context = ctx) {
+    const n = ((width - 2 * rounded) % interval)/2,
+        m = ((height - 2 * rounded) % interval)/2,
+        o = x + width - rounded,
+        p = x + width - radius,
+        q = y + height - radius,
+        r = y + height - rounded;
+    context.save();
+    context.beginPath();
+    context.fillStyle = color;
+    for(let i = n + x + rounded; i <= o; i += interval) {
+        context.moveTo(i - radius, y + radius);
+        context.arc(i, y + radius, radius, 0, 2 * Math.PI, false);
+    }
     
-    ctx.fill();
+    for(let i = n + x + rounded; i <= o; i += interval) {
+        context.moveTo(i - radius, q);
+        context.arc(i, q, radius, 0, 2 * Math.PI, false);
+    }
+    for(let i = m + y + rounded; i <= r; i += interval) {
+        context.moveTo(x + radius, i - radius);
+        context.arc(x + radius, i, radius, 0, Math.PI * 2, false);
+    }
+    for (let i = m + y + rounded; i <= r; i += interval) {
+        context.moveTo(p, i - radius);
+        context.arc(p, i, radius, 0, Math.PI * 2, false);
+    }
+    context.fill();
+    context.restore();
 }
 
 function reset({x = 0, y = 0, width = wid, height = hei, img = init[0]} = {}) {
@@ -54,7 +105,17 @@ function reset({x = 0, y = 0, width = wid, height = hei, img = init[0]} = {}) {
     ctx.fillRect(x, y, width, height);
 }
 
-function initialize (source, callback) {
+function getTouchendPosition(event) {
+
+    let x = event.changedTouches[0].clientX;
+    let y = event.changedTouches[0].clientY;
+
+    return {x, y};
+}
+
+
+// 初始化回调链
+function initialize(source, callback) {
     let n = 3;
 
     for (let [index, url] of source.entries()) {
@@ -66,8 +127,8 @@ function initialize (source, callback) {
                 let bg = ctx.createPattern(init[0], 'repeat');
                 ctx.fillStyle = bg;
                 ctx.fillRect(0, 0, wid, hei);
-                ctx.drawImage(init[1], Math.floor(wid/6 - 25), Math.floor(hei*11/25 + 10));
-                ctx.drawImage(init[2], Math.floor(wid*5/6 - 50), Math.floor(hei*11/25 + 10));
+                ctx.drawImage(init[1], W_6 - 25, H_11_25 + 10);
+                ctx.drawImage(init[2], W_5_6 - 50, H_11_25 + 10);
                 callback();
             }
         }
@@ -76,8 +137,7 @@ function initialize (source, callback) {
 
 function loadImages(source, callback) {
 
-    ctx.fillStyle = '#ffffff';
-    fillRoundedRect(ctx, x_l, y_l, wid_l, hei_l, 5);
+    fillRoundedRect(W_6, H_2_5, W_2_3, H_25);
 
     let loaded = 0,
         num = source.length;
@@ -87,7 +147,7 @@ function loadImages(source, callback) {
         images[index].src = url;
         images[index].onload = function () {
             loaded++;
-            load = (wid_l - 4) * loaded / num;
+            load = (W_2_3 - 4) * loaded / num;
         }
     }
 
@@ -95,21 +155,20 @@ function loadImages(source, callback) {
 }
 
 class ProgressBar {
-    
+
     constructor() {
         this.hue = 0;
         this.width = 10;
         this.loop = true;
     }
-    
+
     draw() {
-        ctx.fillStyle = 'hsla('+this.hue+',100%, 40%, 1)';
-        fillRoundedRect(ctx, x_l + 2, y_l + 2, this.width, hei_l - 4, 5);
-        var grad = ctx.createLinearGradient(x_l + 2, y_l + 2, x_l + wid_l - 4, y_l + hei_l - 4);
+
+        fillRoundedRect(W_6 + 2, H_2_5 + 2, this.width, H_25 - 4, 'hsla(' + this.hue + ',100%, 40%, 1)');
+        var grad = ctx.createLinearGradient(W_6 + 2, H_2_5 + 2, W_6 + W_2_3 - 4, H_2_5 + H_25 - 4);
         grad.addColorStop(0, 'transparent');
         grad.addColorStop(1, 'rgba(0,0,0,0.5)');
-        ctx.fillStyle = grad;
-        fillRoundedRect(ctx, x_l + 2, y_l + 2, this.width, hei_l -4, 5);
+        fillRoundedRect(W_6 + 2, H_2_5 + 2, this.width, H_25 - 4, grad);
     }
 }
 
@@ -118,14 +177,14 @@ let bar = new ProgressBar();
 function loading() {
     bar.hue += 0.8;
 
-    if (bar.width >= wid_l - 4) {
+    if (bar.width >= W_2_3 - 4) {
         bar.loop = false;
-        setTimeout(function() {
+        setTimeout(function () {
             cover();
         }, 300);
     }
 
-    if(bar.width < load && bar.loop) {
+    if (bar.width < load && bar.loop) {
         bar.width += 2;
         bar.draw();
         requestAnimationFrame(loading);
@@ -147,15 +206,15 @@ function cover() {
 
         draw() {
             ctx.save();
-            ctx.translate(0, hei*7/10);
-            ctx.rotate(15*Math.PI/180);
-            ctx.drawImage(images[1], 0, 0, wid/4 + this.size, wid/4 + this.size);
+            ctx.translate(0, H_7_10);
+            ctx.rotate(15 * Math.PI / 180);
+            ctx.drawImage(images[1], 0, 0, W_4 + this.size, W_4 + this.size);
             ctx.restore();
             ctx.save();
-            ctx.translate(wid/15, hei * 45/64);
-            ctx.rotate(-20*Math.PI/180);
+            ctx.translate(W_15, H_45_64);
+            ctx.rotate(-20 * Math.PI / 180);
             ctx.globalAlpha = 0.5;
-            ctx.drawImage(images[1], 0, 0, wid/7 + this.size/2, wid / 7 + this.size/2);
+            ctx.drawImage(images[1], 0, 0, W_7 + this.size / 2, W_7 + this.size / 2);
             ctx.globalAlpha = 1;
             ctx.restore();
         }
@@ -165,16 +224,16 @@ function cover() {
 
     function animloop() {
         if (hea.loop) {
-            reset({x: 0, y: hei*11/16, width: wid/4, height: hei*5/16});
+            reset({x: 0, y: H_11_16, width: W_4, height: H_5_16});
             if (hea.state == 0 && hea.size > 10) {
                 hea.state = 1;
             }
             if (hea.state == 1 && hea.size <= 0) {
                 hea.state = 0;
             }
-            if(hea.state == 1) {
+            if (hea.state == 1) {
                 hea.size -= 0.5;
-            } else if(hea.state == 0) {
+            } else if (hea.state == 0) {
                 hea.size += 0.5;
             }
             hea.draw();
@@ -185,41 +244,107 @@ function cover() {
     function coverHandler(event) {
         event.preventDefault();
         hea.loop = false;
-        console.log('tab');
         firstPage();
         cav.removeEventListener('touchend', coverHandler, false);
     }
 
     reset();
-    ctx.drawImage(images[3], wid - hei/742*400, 0, hei/742*400, hei);
+    ctx.drawImage(images[3], wid - H_200_371, 0, H_200_371, hei);
     ctx.save();
-    ctx.translate(0, hei/30);
-    ctx.rotate(-30*Math.PI/180);
-    ctx.drawImage(images[1], 0, 0, wid/4, wid/4);
+    ctx.translate(0, hei / 30);
+    ctx.rotate(-30 * Math.PI / 180);
+    ctx.drawImage(images[1], 0, 0, W_4, W_4);
     ctx.restore();
     ctx.save();
-    ctx.translate(wid*7/26, hei*5/24);
-    ctx.rotate(30*Math.PI/180);
+    ctx.translate(W_7_26, H_5_24);
+    ctx.rotate(30 * Math.PI / 180);
     ctx.globalAlpha = 0.5;
-    ctx.drawImage(images[1], 0, 0, wid/6, wid/6);
+    ctx.drawImage(images[1], 0, 0, W_6, W_6);
     ctx.globalAlpha = 1;
     ctx.restore();
-    ctx.drawImage(images[2], 0, 0, wid*5/12, hei*7/10);
+    ctx.drawImage(images[2], 0, 0, W_5_12, H_7_10);
     animloop();
 
     cav.addEventListener('touchend', coverHandler, false);
 }
 
+// 问卷选择页面。
+
+class QueryPage {
+    constructor(arr, style, source, statement = false) {
+        this.arr = arr;
+        this.style = style;
+        this.source = source;
+        this.option = null;
+        this.context = ctx;
+        this.statement = statement;
+    }
+
+    draw(point) {
+        for (let [index, arg] of this.arr.entries()) {
+            fillRoundedRect(arg.x, arg.y, arg.width, arg.height, this.style[index]);
+            if (!this.statement || index > 0) {
+                if (point && this.context.isPointInPath(point.x, point.y)) {
+                    this.option = index;
+                }
+            }
+            roundedRectDot(arg.x, arg.y, arg.width, arg.height, 5, '#E0F8A9', 2, 6);
+            this.context.drawImage(this.source[index], arg.x, arg.y, arg.width, arg.height);
+        }
+    }
+}
+
 function firstPage() {
 
+    function handler(event) {
+        event.preventDefault();
+        let p = getTouchendPosition(event);
+        fir.draw(p);
+        switch (fir.option) {
+            case 1:
+                window.result.gender = 'female';
+                firstLadyPage();
+                cav.removeEventListener('touchend', handler, false);
+                break;
+            case 0:
+                window.result.gender = 'male';
+                firstManPage();
+                cav.removeEventListener('touchend', handler, false);
+                break;
+            default:
+                fir.option = null;
+        }
+    }
+
+    let fir = new QueryPage([{x: W_10, y: H_5_18, width: W_4_5, height: H_11_60},
+            {x: W_10, y: H_2, width: W_4_5, height: H_11_60}], ['#7493C9', '#F376A2'],
+            [images[4],images[5]]);
+
+    reset();
+    fir.draw();
+
+    cav.addEventListener('touchend', handler, false);
+}
+
+function firstManPage() {
+    reset({img: images[0]});
+}
+
+function firstLadyPage() {
     reset();
 }
 
-window.onload = function() {
 
-    let source = ['./img/bg_1.jpg','./img/heart.png', './img/main-title.png', './img/oba.png'];
-    initialize(['./img/bg.jpg', './img/0.png', './img/100.png'], function() {
-        loadImages(source, function() {
+
+
+
+// 页面初始化
+window.onload = function () {
+
+    let source = ['./img/bg_1.png', './img/heart.png', './img/main-title.png', './img/oba.png',
+            './img/1_1.png', './img/1_2.png'];
+    initialize(['./img/bg.jpg', './img/0.png', './img/100.png'], function () {
+        loadImages(source, function () {
             loading();
         });
     });
--- js/main.js
+++ js/main.js
@@ -6,32 +6,45 @@ var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = [
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+var W_4 = Math.floor(wid / 4),
+    W_6 = Math.floor(wid / 6),
+    W_7 = Math.floor(wid / 7),
+    W_10 = Math.floor(wid / 10),
+    W_15 = Math.floor(wid / 15),
+    W_2_3 = Math.floor(wid * 2 / 3),
+    W_4_5 = Math.floor(wid * 4 / 5),
+    W_5_6 = Math.floor(wid * 5 / 6),
+    W_7_26 = Math.floor(wid * 7 / 26),
+    W_5_12 = Math.floor(wid * 5 / 12),
+    H_2 = Math.floor(hei / 2),
+    H_25 = Math.floor(hei / 25),
+    H_2_5 = Math.floor(hei * 2 / 5),
+    H_7_10 = Math.floor(hei * 7 / 10),
+    H_11_16 = Math.floor(hei * 11 / 16),
+    H_5_16 = Math.floor(hei * 5 / 16),
+    H_5_18 = Math.floor(hei * 5 / 18),
+    H_5_24 = Math.floor(hei * 5 / 24),
+    H_11_25 = Math.floor(hei * 11 / 25),
+    H_11_60 = Math.floor(hei * 11 / 60),
+    H_45_64 = Math.floor(hei * 45 / 64),
+    H_200_371 = Math.floor(hei * 200 / 371);
+
+// 公共变量和一些常量。
 var images = [],
     init = [],
-    resule = 0,
+    result = {},
     load = 0,
     ctx = cav.getContext('2d');
 
-var x_l = Math.floor(wid / 6),
-    y_l = Math.floor(hei * 2 / 5),
-    wid_l = Math.floor(wid * 2 / 3),
-    hei_l = Math.floor(hei / 25);
-
+// 工具函数
 window.requestAnimFrame = function () {
     return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
         window.setTimeout(callback, 1000 / 60);
     };
 }();
 
-function getTouchendPosition(event) {
+function drawRoundedRect(x, y, width, height, radius, context) {
 
-    var x = event.changedTouches[0].clientX;
-    var y = event.changedTouches[0].clientY;
-
-    return { x: x, y: y };
-}
-
-function fillRoundedRect(context, x, y, width, height, radius) {
     context.beginPath();
 
     if (width > 0) {
@@ -39,16 +52,59 @@ function fillRoundedRect(context, x, y, width, height, radius) {
     } else {
         context.moveTo(x - radius, y);
     }
+
     context.arcTo(x + width, y, x + width, y + height, radius);
     context.arcTo(x + width, y + height, x, y + height, radius);
     context.arcTo(x, y + height, x, y, radius);
+
     if (width > 0) {
         context.arcTo(x, y, x + width, y, radius);
     } else {
         context.arcTo(x, y, x - width, y, radius);
     }
+}
 
-    ctx.fill();
+function fillRoundedRect(x, y, width, height) {
+    var style = arguments.length <= 4 || arguments[4] === undefined ? "#ffffff" : arguments[4];
+    var radius = arguments.length <= 5 || arguments[5] === undefined ? 5 : arguments[5];
+    var context = arguments.length <= 6 || arguments[6] === undefined ? ctx : arguments[6];
+
+    context.fillStyle = style;
+    drawRoundedRect(x, y, width, height, radius, context);
+    context.fill();
+}
+
+function roundedRectDot(x, y, width, height, rounded, color, radius, interval) {
+    var context = arguments.length <= 8 || arguments[8] === undefined ? ctx : arguments[8];
+
+    var n = (width - 2 * rounded) % interval / 2,
+        m = (height - 2 * rounded) % interval / 2,
+        o = x + width - rounded,
+        p = x + width - radius,
+        q = y + height - radius,
+        r = y + height - rounded;
+    context.save();
+    context.beginPath();
+    context.fillStyle = color;
+    for (var i = n + x + rounded; i <= o; i += interval) {
+        context.moveTo(i - radius, y + radius);
+        context.arc(i, y + radius, radius, 0, 2 * Math.PI, false);
+    }
+
+    for (var _i = n + x + rounded; _i <= o; _i += interval) {
+        context.moveTo(_i - radius, q);
+        context.arc(_i, q, radius, 0, 2 * Math.PI, false);
+    }
+    for (var _i2 = m + y + rounded; _i2 <= r; _i2 += interval) {
+        context.moveTo(x + radius, _i2 - radius);
+        context.arc(x + radius, _i2, radius, 0, Math.PI * 2, false);
+    }
+    for (var _i3 = m + y + rounded; _i3 <= r; _i3 += interval) {
+        context.moveTo(p, _i3 - radius);
+        context.arc(p, _i3, radius, 0, Math.PI * 2, false);
+    }
+    context.fill();
+    context.restore();
 }
 
 function reset() {
@@ -70,6 +126,15 @@ function reset() {
     ctx.fillRect(x, y, width, height);
 }
 
+function getTouchendPosition(event) {
+
+    var x = event.changedTouches[0].clientX;
+    var y = event.changedTouches[0].clientY;
+
+    return { x: x, y: y };
+}
+
+// 初始化回调链
 function initialize(source, callback) {
     var n = 3;
 
@@ -92,8 +157,8 @@ function initialize(source, callback) {
                     var bg = ctx.createPattern(init[0], 'repeat');
                     ctx.fillStyle = bg;
                     ctx.fillRect(0, 0, wid, hei);
-                    ctx.drawImage(init[1], Math.floor(wid / 6 - 25), Math.floor(hei * 11 / 25 + 10));
-                    ctx.drawImage(init[2], Math.floor(wid * 5 / 6 - 50), Math.floor(hei * 11 / 25 + 10));
+                    ctx.drawImage(init[1], W_6 - 25, H_11_25 + 10);
+                    ctx.drawImage(init[2], W_5_6 - 50, H_11_25 + 10);
                     callback();
                 }
             };
@@ -116,8 +181,7 @@ function initialize(source, callback) {
 
 function loadImages(source, callback) {
 
-    ctx.fillStyle = '#ffffff';
-    fillRoundedRect(ctx, x_l, y_l, wid_l, hei_l, 5);
+    fillRoundedRect(W_6, H_2_5, W_2_3, H_25);
 
     var loaded = 0,
         num = source.length;
@@ -137,7 +201,7 @@ function loadImages(source, callback) {
             images[index].src = url;
             images[index].onload = function () {
                 loaded++;
-                load = (wid_l - 4) * loaded / num;
+                load = (W_2_3 - 4) * loaded / num;
             };
         }
     } catch (err) {
@@ -170,13 +234,12 @@ var ProgressBar = function () {
     _createClass(ProgressBar, [{
         key: 'draw',
         value: function draw() {
-            ctx.fillStyle = 'hsla(' + this.hue + ',100%, 40%, 1)';
-            fillRoundedRect(ctx, x_l + 2, y_l + 2, this.width, hei_l - 4, 5);
-            var grad = ctx.createLinearGradient(x_l + 2, y_l + 2, x_l + wid_l - 4, y_l + hei_l - 4);
+
+            fillRoundedRect(W_6 + 2, H_2_5 + 2, this.width, H_25 - 4, 'hsla(' + this.hue + ',100%, 40%, 1)');
+            var grad = ctx.createLinearGradient(W_6 + 2, H_2_5 + 2, W_6 + W_2_3 - 4, H_2_5 + H_25 - 4);
             grad.addColorStop(0, 'transparent');
             grad.addColorStop(1, 'rgba(0,0,0,0.5)');
-            ctx.fillStyle = grad;
-            fillRoundedRect(ctx, x_l + 2, y_l + 2, this.width, hei_l - 4, 5);
+            fillRoundedRect(W_6 + 2, H_2_5 + 2, this.width, H_25 - 4, grad);
         }
     }]);
 
@@ -188,7 +251,7 @@ var bar = new ProgressBar();
 function loading() {
     bar.hue += 0.8;
 
-    if (bar.width >= wid_l - 4) {
+    if (bar.width >= W_2_3 - 4) {
         bar.loop = false;
         setTimeout(function () {
             cover();
@@ -219,15 +282,15 @@ function cover() {
             key: 'draw',
             value: function draw() {
                 ctx.save();
-                ctx.translate(0, hei * 7 / 10);
+                ctx.translate(0, H_7_10);
                 ctx.rotate(15 * Math.PI / 180);
-                ctx.drawImage(images[1], 0, 0, wid / 4 + this.size, wid / 4 + this.size);
+                ctx.drawImage(images[1], 0, 0, W_4 + this.size, W_4 + this.size);
                 ctx.restore();
                 ctx.save();
-                ctx.translate(wid / 15, hei * 45 / 64);
+                ctx.translate(W_15, H_45_64);
                 ctx.rotate(-20 * Math.PI / 180);
                 ctx.globalAlpha = 0.5;
-                ctx.drawImage(images[1], 0, 0, wid / 7 + this.size / 2, wid / 7 + this.size / 2);
+                ctx.drawImage(images[1], 0, 0, W_7 + this.size / 2, W_7 + this.size / 2);
                 ctx.globalAlpha = 1;
                 ctx.restore();
             }
@@ -240,7 +303,7 @@ function cover() {
 
     function animloop() {
         if (hea.loop) {
-            reset({ x: 0, y: hei * 11 / 16, width: wid / 4, height: hei * 5 / 16 });
+            reset({ x: 0, y: H_11_16, width: W_4, height: H_5_16 });
             if (hea.state == 0 && hea.size > 10) {
                 hea.state = 1;
             }
@@ -260,39 +323,130 @@ function cover() {
     function coverHandler(event) {
         event.preventDefault();
         hea.loop = false;
-        console.log('tab');
         firstPage();
         cav.removeEventListener('touchend', coverHandler, false);
     }
 
     reset();
-    ctx.drawImage(images[3], wid - hei / 742 * 400, 0, hei / 742 * 400, hei);
+    ctx.drawImage(images[3], wid - H_200_371, 0, H_200_371, hei);
     ctx.save();
     ctx.translate(0, hei / 30);
     ctx.rotate(-30 * Math.PI / 180);
-    ctx.drawImage(images[1], 0, 0, wid / 4, wid / 4);
+    ctx.drawImage(images[1], 0, 0, W_4, W_4);
     ctx.restore();
     ctx.save();
-    ctx.translate(wid * 7 / 26, hei * 5 / 24);
+    ctx.translate(W_7_26, H_5_24);
     ctx.rotate(30 * Math.PI / 180);
     ctx.globalAlpha = 0.5;
-    ctx.drawImage(images[1], 0, 0, wid / 6, wid / 6);
+    ctx.drawImage(images[1], 0, 0, W_6, W_6);
     ctx.globalAlpha = 1;
     ctx.restore();
-    ctx.drawImage(images[2], 0, 0, wid * 5 / 12, hei * 7 / 10);
+    ctx.drawImage(images[2], 0, 0, W_5_12, H_7_10);
     animloop();
 
     cav.addEventListener('touchend', coverHandler, false);
 }
 
+// 问卷选择页面。
+
+var QueryPage = function () {
+    function QueryPage(arr, style, source) {
+        var statement = arguments.length <= 3 || arguments[3] === undefined ? false : arguments[3];
+
+        _classCallCheck(this, QueryPage);
+
+        this.arr = arr;
+        this.style = style;
+        this.source = source;
+        this.option = null;
+        this.context = ctx;
+        this.statement = statement;
+    }
+
+    _createClass(QueryPage, [{
+        key: 'draw',
+        value: function draw(point) {
+            reset();
+            var _iteratorNormalCompletion3 = true;
+            var _didIteratorError3 = false;
+            var _iteratorError3 = undefined;
+
+            try {
+                for (var _iterator3 = this.arr.entries()[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+                    var _step3$value = _slicedToArray(_step3.value, 2);
+
+                    var index = _step3$value[0];
+                    var arg = _step3$value[1];
+
+                    fillRoundedRect(arg.x, arg.y, arg.width, arg.height, this.style[index]);
+                    if (!this.statement || index > 0) {
+                        if (point && this.context.isPointInPath(point.x, point.y)) {
+                            this.option = index;
+                        }
+                    }
+                    roundedRectDot(arg.x, arg.y, arg.width, arg.height, 5, '#E0F8A9', 2, 6);
+                    this.context.drawImage(this.source[index], arg.x, arg.y, arg.width, arg.height);
+                }
+            } catch (err) {
+                _didIteratorError3 = true;
+                _iteratorError3 = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion3 && _iterator3.return) {
+                        _iterator3.return();
+                    }
+                } finally {
+                    if (_didIteratorError3) {
+                        throw _iteratorError3;
+                    }
+                }
+            }
+        }
+    }]);
+
+    return QueryPage;
+}();
+
 function firstPage() {
 
+    function handler(event) {
+        event.preventDefault();
+        var p = getTouchendPosition(event);
+        fir.draw(p);
+        switch (fir.option) {
+            case 1:
+                window.result.gender = 'female';
+                firstLadyPage();
+                cav.removeEventListener('touchend', handler, false);
+                break;
+            case 0:
+                window.result.gender = 'male';
+                firstManPage();
+                cav.removeEventListener('touchend', handler, false);
+                break;
+            default:
+                fir.option = null;
+        }
+    }
+
+    var fir = new QueryPage([{ x: W_10, y: H_5_18, width: W_4_5, height: H_11_60 }, { x: W_10, y: H_2, width: W_4_5, height: H_11_60 }], ['#7493C9', '#F376A2'], [images[4], images[5]]);
+    fir.draw();
+
+    cav.addEventListener('touchend', handler, false);
+}
+
+function firstManPage() {
+    reset({ img: images[0] });
+}
+
+function firstLadyPage() {
     reset();
 }
 
+// 页面初始化
 window.onload = function () {
 
-    var source = ['./img/bg_1.jpg', './img/heart.png', './img/main-title.png', './img/oba.png'];
+    var source = ['./img/bg_1.png', './img/heart.png', './img/main-title.png', './img/oba.png', './img/1_1.png', './img/1_2.png'];
     initialize(['./img/bg.jpg', './img/0.png', './img/100.png'], function () {
         loadImages(source, function () {
             loading();
